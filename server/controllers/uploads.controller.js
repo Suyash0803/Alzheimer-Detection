cadbb4f0cd5e827971e491b0bc4aa52fc4654b9a
sanjana-19o8 <sanjanavajrapu19@gmail.com>
2024-05-01 12:32:51 +0530

    transcripting

--- server/controllers/uploads.controller.js
+++ server/controllers/uploads.controller.js
@@ -1,14 +1,31 @@
 import fs from 'fs';
+import { getTranscripts } from '../utils/textconverter.js';
 
-const saveAudio = (req, res) => {
+async function generateTranscripts( audioUrl ) {
+    try {
+        const transcript = await getTranscripts(audioUrl);
+        // save transcript as json
+        const transcriptJson = JSON.stringify(transcript)
+        fs.writeFileSync('./cache/transcript.json', transcriptJson)
+        console.log('Trancript file saved successfully')
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+function saveAudio(req, res) {
     try {
         const { path } = req.file;
-        fs.renameSync(path, `uploads/${req.file.filename}`);
-        res.json({ message: 'Audio saved successfully' });
+        const newpath = `uploads/${req.file.originalname}`
+        fs.renameSync(path, newpath);
+        generateTranscripts(newpath);
+        
+        res.json({ message: 'Audio saved successfully', file: req.file });
+
     } catch (err) {
-        console.error(err)
-        return res.status(500).json({message: "Audio upload failed. Please try again"})
+        console.error(err);
+        return res.status(500).json({ message: "Audio upload failed. Please try again" });
     }
-};
+}
 
 export { saveAudio };
